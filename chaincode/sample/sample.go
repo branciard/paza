bbbc4d2aa0d218b02fb331298c4077a7894d4599
branciard <francoisbranciard@gmail.com>
2016-11-09 17:20:43 +0100

    test chain code

--- chaincode/sample/sample.go
+++ chaincode/sample/sample.go
@@ -1,11 +1,28 @@
-
+/*
+Copyright IBM Corp. 2016 All Rights Reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+		 http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package main
 
-//WARNING - this chaincode's ID is hard-coded in chaincode_example04 to illustrate one way of
-//calling chaincode from a chaincode. If this example is modified, chaincode_example04.go has
-//to be modified as well with the new ID of chaincode_example02.
-//chaincode_example05 show's how chaincode ID can be passed in as a parameter instead of
-//hard-coding.
+import (
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+	"github.com/hyperledger/fabric/core/errors"
+)
+
+
+
+
+
+// the orginal code is here. https://github.com/hyperledger/fabric/blob/master/examples/chaincode/go/chaincode_example02/chaincode_example02.go
+// the goal of this chaincode is just to test if the project environement  works and is up and running. ready to dev ...
 
 import (
 	"errors"
@@ -16,10 +33,6 @@ import (
 )
 
 
-
-
-
-// SimpleChaincode example simple Chaincode implementation
 type SimpleChaincode struct {
 }
 
